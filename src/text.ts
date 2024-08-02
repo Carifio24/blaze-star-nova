a878023a3d5373c3409bf1b2bca9e18e83d04b67
Carifio24 <carifio24@gmail.com>
2024-08-02 17:53:57 -0400

    WIP: Start working on adding text overlays in sky.

---
+++ src/text.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+/* eslint-disable */
+
+import { Coordinates, Text3d, Text3dBatch } from "@wwtelescope/engine";
+
+export function makeOverlayText() {
+  const glyphHeight = 50;
+  const textBatch = new Text3dBatch(glyphHeight);
+
+}
